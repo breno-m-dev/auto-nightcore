3417f6d213ef43510203f8d2748e0eccdec29ef7
Breno Macedo B. de Souza <brenomdev@protonmail.com>
2025-11-08 21:02:24 -0300

    -added function to auto_video_editor.py to randomly choose an image, from a local folder, to be used as background image to the video -Added TEST_auto_video_editor.py to test auto_video_editor.py's functionality

---
+++ TEST_auto_video_editor.py
@@ -0,0 +1,5 @@
+import auto_video_editor
+
+#print( auto_video_editor.random_image("./images") )
+
+auto_video_editor.make_video("night_core_taste_the_blood.mp3", True)
--- auto_video_editor.py
+++ auto_video_editor.py
@@ -1,9 +1,10 @@
 from moviepy import *
+import os
+import random
 
 
-
-
-def make_video(input_audio: str, input_img: str ):
+#input_img is a optional parameter. It will only be used if random_image == false
+def make_video(input_audio: str, use_random_image: bool , input_img: str = None):
     
    
 
@@ -11,10 +12,24 @@ def make_video(input_audio: str, input_img: str ):
     audio = AudioFileClip(input_audio)
 
     # Carregar imagem (png/jpg)
-    image = ImageClip(input_img, duration= audio.duration)  # duração em segundos
+    if(use_random_image):
+        video_image = random_image("./images")
+        image = ImageClip(video_image, duration= audio.duration)  # duração em segundos
+    else:
+        image = ImageClip(input_img, duration= audio.duration)  # duração em segundos
 
     # Definir o áudio do vídeo
     video = image.with_audio(audio)
     
     # Exportar vídeo
     video.write_videofile("nightcoreExport.mp4", fps=24, codec="libx264", audio_codec="aac")
+
+
+def random_image(folder: str):
+    imagens = [
+        nome for nome in os.listdir(folder)
+        if os.path.isfile(os.path.join(folder, nome))
+    ]
+    #print(imagens)
+    
+    return folder+"/"+random.choice(imagens)
