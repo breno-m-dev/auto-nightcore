60c4d81221f9f4ba4d9278e141dba99808f5ebf2
Breno Macedo B. de Souza <brenomdev@protonmail.com>
2026-03-03 15:54:34 -0300

    -refactored youtube_download.py

---
+++ test_name_sanitizer.py
@@ -0,0 +1,5 @@
+import youtube_download
+
+
+print(youtube_download.sanitize_filename("Demon's Souls Soundtrack - \"Tower Knight/Penetrator\""))
+    
--- youtube_download.py
+++ youtube_download.py
@@ -3,7 +3,7 @@ from pytubefix import YouTube, Playlist
 from pytubefix.cli import on_progress
 from pytubefix.helpers import safe_filename
 import re
-downloaded_music = "./downloaded_music"
+DOWNLOAD_FOLDER= "./downloaded_music"
 
 class Musica:
     def __init__(self, path, title):
@@ -15,14 +15,22 @@ class Musica:
 
 
 lista_musicas = []
-lonely_music = Musica("","")
+#lonely_music = Musica("","")
 
 
-def sanitize_filename(old_filename: str) -> str:
+def clean_filename(old_filename: str) -> str:
     """
-    remove characters that would generate errors
-    this was found trying to make a nightcore song out of a song with the following title:
-    Demon's Souls Soundtrack - "Tower Knight/Penetrator"
+    Remove characters that would generate errors due to reserved symbols in the OS..
+    This was found trying to make a nightcore song out of a song with the following title:
+    Demon's Souls Soundtrack - "Tower Knight/Penetrator".
+    Args:
+        old_filename (str): Name of the chosen file.
+    Returns:
+        str: cleaned version of the file name.
+
+    Example:
+        >>> clean_filename("Demon's Souls Soundtrack - \"Tower Knight/Penetrator\"")
+        "Returns: 'Demonssoulssoundtrac-towerknightpenetrator"
     """
   
     new_filename = re.sub(r'[<>:"/\\|?*]', '', old_filename )
@@ -30,41 +38,64 @@ def sanitize_filename(old_filename: str) -> str:
     new_filename = re.sub(r" ", "", new_filename)
     return new_filename
 
+#started using type hints, even though it doesnt
+#change execution. Just because it will help me later on
+def download_audio_from_url(url: str) -> Musica:
+    """
+    Download audio from a single YouTube video from given URL.
 
-def download_audio_from_url(url):
+    Args:
+        url (str): URL of the YouTube video.
+
+    Returns:
+        Musica: Object containing the downloaded file path and title.
+
+    Raises:
+        Exception: If connection or download fails.
     """
-    Download the audio from a youtube video through its URL
-    """
-    #following try tests if the URL is valid and if the connection is functioning
+
     try:
         yt = YouTube(url, on_progress_callback=on_progress)
         
     except:
         print("Connection or URL Error")
     else:
-        if not os.path.exists(downloaded_music):
-            os.makedirs(downloaded_music)
-        folder_name = downloaded_music
+        if not os.path.exists(DOWNLOAD_FOLDER):
+            os.makedirs(DOWNLOAD_FOLDER)
+        folder_name = DOWNLOAD_FOLDER
         #print(yt.title)
         
-        safe_title = dowload_audio_from_object(yt, folder_name)
-        lonely_music.set_all(folder_name+"/"+safe_title+".m4a", safe_title)
-    
+        safe_title = download_audio_from_object(yt, folder_name)
+        our_music = Musica( os.path.join(folder_name, f"{safe_title}.m4a"),
+                           safe_title
+        )
+        
+        return our_music
 
 
-def dowload_audio_from_object(yt, outpath) -> str:
+
+def download_audio_from_object(yt: YouTube, outpath: str) -> str:
     """
-    Baixa o áudio de um objeto do tipo YouTube.
+    Download music from a YouTube object.
+    Args:
+        yt (YouTube): YouTube object representing a video and its metadata.
+        outpath (str): path to where the video shall be downloaded into.
+    
+    Returns:
+        str: Sanitized title used as the downloaded file name.
+        This is returned, just in case the yt.title differs from the cleaned version.
+    
+    Raises:
+        Exception: If the download process fails.
     """
     print(yt.title)
     print("Downloading...")
-    safe_name = sanitize_filename(yt.title)
+    safe_name = clean_filename(yt.title)
     try:
         ys = yt.streams.get_audio_only()
         ys.download(output_path = outpath, filename = safe_name+".m4a")
         yt.title = safe_name
 
-        #ys.download()
     except:
         print("An download error has occured")
     else:
@@ -87,7 +118,7 @@ def download_audio_playlist(url: str):
             os.makedirs(playlist_folder)
 
         print(f"Salvando os arquivos em: {os.path.abspath(playlist_folder)}")
-        lista_musicas.clear()
+        lista_musicas = []
         
         for url in playlist.video_urls:
             print("-" * 40)
@@ -95,15 +126,28 @@ def download_audio_playlist(url: str):
             print(playlist_folder)
             yt_temp = YouTube(url, on_progress_callback=on_progress)
 
-            safe_title = dowload_audio_from_object(yt_temp, playlist_folder)
+            safe_title = download_audio_from_object(yt_temp, playlist_folder)
             lista_musicas.append( Musica( playlist_folder+"/"+safe_title+".m4a" , safe_title))
 
         print("\n✅ Playlist download completed.")
-
+        return lista_musicas;
     except Exception as e:
         print(f"❌ Ocurred an error with the playlist {url}: {e} Insert link of playlist, not the video!")
 
-def is_playlist(url):
+def is_playlist(url: str) -> bool:
+    """
+    Checks if the video from an url is a single video or a playlist
+    
+    Args:
+        url (str): the url of a youtube video or youtube playlist.
+    
+    Returns:
+        True if the URL represents a playlist, False otherwise.
+    
+    Raises:
+        Exception: if the video is unavaible. 
+        Exception: if the link is not link from a youtube video or playlist.
+    """
     try:
         p = Playlist(url)
         if len(p.video_urls) > 0:
@@ -113,23 +157,3 @@ def is_playlist(url):
     except Exception:
         return False
 
-#print(sanitize_filename ('a','Demon\'s Souls Soundtrack - "Tower Knight/Penetrator"') )
-
-# url = input("Enter the YouTube video or playlist URL: ")
-# download_audio_from_url(url)
-# print(is_playlist(url))
-
-# def main():
-#     """
-#     Função principal para lidar com a entrada do usuário e decidir se
-#     baixa um único vídeo ou uma playlist.
-#     """
-#     url = input("Enter the YouTube video or playlist URL: ")
-
-#     if "playlist?list=" in url:
-#         download_audio_playlist(url)
-#     else:
-#         download_audio(url)
-
-# if __name__ == "__main__":
-#     main()
